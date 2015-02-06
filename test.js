2d2ba9c3963da1d5150f993f40de29702f68f80d
quim <joaquim.serafim@gmail.com>
2015-02-06 17:10:41 +0000

    v1

--- test.js
+++ test.js
@@ -3,14 +3,14 @@
 var test        = require('tape');
 var isJsObject  = require('./');
 
-test('should be a valid JS Object', function(assert) {
+test('should test valids JS Objects', function(assert) {
   assert.deepEqual(isJsObject({a: 1}), true);
   /*jshint -W010 */
   assert.deepEqual(isJsObject(new Object()), true);
   assert.end();
 });
 
-test('should test invalids JS Object', function(assert) {
+test('should test invalids JS Objects', function(assert) {
   /*jshint -W009 */
   ['1', 1, null, undefined, [], false, Object, new Array()]
     .forEach(function(value) {
